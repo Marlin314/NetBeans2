9926e67138e00fec9b2f79d744ae05bfcb853c77
Marlin Eller <meller@uw.edu>
2015-08-20 13:47:30 -0700

    dude

---
+++ src/netbeans2/NetBeans2.java
@@ -0,0 +1,19 @@
+/*
+ * copyright 2015 Marlin Eller meller at uw dot edu
+ */
+package netbeans2;
+
+/**
+ *
+ * @author meller
+ */
+public class NetBeans2 {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("NetBeans2");
+  }
+  
+}
